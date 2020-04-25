a314070d3220bd314975f2877118e771493c0207
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-04-26 00:16:01 +0800

    chore: add ping2

--- main.go
+++ main.go
@@ -13,5 +13,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/ping2", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong2",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
